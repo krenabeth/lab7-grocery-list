02c5bc84b5776a0d941ecb25cc6a4a53c840f577
Karen Langner <krenabeth@yahoo.com>
2016-07-07 13:02:12 -0400

    initial commit, js attempt is commented out

---
+++ script.js
@@ -0,0 +1,75 @@
+var veg = {
+  name: 'broccoli',
+  price: 2.00
+};
+
+var fruit = {
+  name: 'raspberries',
+  price: 4.50
+};
+
+var dairy = {
+  name: 'eggs',
+  price: 3.00
+};
+
+var list;
+list = [veg, fruit, dairy];
+
+var price = 0;
+list.forEach(function(groceries){
+  console.log(groceries.name);
+  console.log(groceries.price);
+
+  price += groceries.price;
+});
+
+             
+console.log("total = " + price);
+
+//  attempt to get answers into the html
+
+// 		"<li></li>" +
+// 		"<li></li>" +
+// 	"</ul>";
+// document.getElementById("evenSnazzier").innerHTML =
+// 	"<ul id='bucks'>" +
+// 		"<li></li>" +
+// 		"<li></li>" +
+// 		"<li></li>" +
+// 	"</ul>";
+
+
+// var veg = {
+//   name: 'broccoli',
+//   price: 2.00
+// };
+
+// var fruit = {
+//   name: 'raspberries',
+//   price: 4.50
+// };
+
+// var dairy = {
+//   name: 'eggs',
+//   price: 3.00
+// };
+
+// var list;
+// list = [veg, fruit, dairy];
+
+// var price = 0;
+// list.forEach(function(groceries){
+// 	document.getElementById("snazzy").innerHTML =
+// 	"<ul class='eats'>" + "<li>" + groceries.name + "</li>";
+// 	document.getElementById("evenSnazzier").innerHTML =
+// 	"<ul class='bucks'>" + "<li>" + groceries.price + "</li>"; // what's going on with price?
+//   	price += groceries.price;
+// });
+// document.getElementById("snazzy").innerHTML =
+//  "<h4>Total = </h4>";  
+
+// document.getElementById("evenSnazzier").innerHTML =          
+// "<h4>" + price + "</h4>";
+
+
